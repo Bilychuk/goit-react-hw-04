d01d756f76bd83fd9f77436d993c05c99a1c9e7a
Bilychuk Kateryna <bilychukkateryna@ukr.net>
2024-04-23 14:12:24 +0300

    fix ImageCard

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,17 +2,20 @@ import css from './ImageCard.module.css';
 
 export default function ImageCard({ onImageClick, image }) {
   return (
-    <div
-      onClick={() =>
-        onImageClick(
-          image.urls.regular,
-          image.description,
-          image.user.name,
-          image.likes
-        )
-      }
-    >
-      <img className={css.img} src={image.urls.small} alt={image.description} />
+    <div>
+      <img
+        className={css.img}
+        src={image.urls.small}
+        alt={image.description}
+        onClick={() =>
+          onImageClick(
+            image.urls.regular,
+            image.description,
+            image.user.name,
+            image.likes
+          )
+        }
+      />
     </div>
   );
 }
